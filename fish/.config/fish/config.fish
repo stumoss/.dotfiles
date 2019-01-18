d015eddffc259f83900ccff9acf7d9e3b2cf8ec4
Stuart Moss <samoss@gmail.com>
2019-01-18 19:53:29 +0000

    Update fish config to alias some commands

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,9 @@
-set -gx PATH $HOME/.cargo/bin $HOME/Code/Go/bin $PATH
-set -gx GOPATH $HOME/Code/Go
+set -gx PATH $HOME/.cargo/bin $PATH
 
+function tmux
+    command tmux -f $HOME/.config/tmux/tmux.conf $argv
+end
+
+function pass
+    env PASSWORD_STORE_DIR=$HOME/.config/password-store pass $argv
+end
