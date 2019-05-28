96f11db8d7ec62a4821cb8046a7fca1e65f82d8c
Stuart Moss <samoss@gmail.com>
2019-05-28 21:27:28 +0100

    Update nix-build-callpakage

--- nixpkgs/.config/nixpkgs/overlays/nix-tools.nix
+++ nixpkgs/.config/nixpkgs/overlays/nix-tools.nix
@@ -4,7 +4,7 @@ self: super:
     nix-build-callpackage = super.writeScriptBin "nix-build-callpackage"
     ''
       #!${super.bash}/bin/bash -p
-      exec nix-build -E "with import <nixpkgs> {}; callPackage $(realpath ''${1}) {}"
+      exec nix-build ''${@:1:$#-1} -E "with import <nixpkgs> {}; callPackage $(realpath ''${!#}) {}"
     '';
 
     nix-rebuild = super.writeScriptBin "nix-rebuild"
